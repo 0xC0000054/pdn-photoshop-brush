867e11e908429644cff472980886556f0cc80721
Nicholas Hayes <0xC0000054@users.noreply.github.com>
2018-12-04 21:23:53 -0700

    Remove the dependency on BinaryWriter
    This eliminates any chance of accidentally using a BinaryWriter method
    that was not overridden to support big-endian data.
    It also removes the need to perform byte-swapping on the primitive
    types, which may improve performance.

--- BigEndianBinaryWriter.cs
+++ BigEndianBinaryWriter.cs
@@ -26,138 +26,348 @@
 //
 /////////////////////////////////////////////////////////////////////////////////
 
+using System;
 using System.Drawing;
 using System.IO;
 using System.Text;
 
 namespace AbrFileTypePlugin
 {
-    internal sealed class BigEndianBinaryWriter : BinaryWriter
+    internal sealed class BigEndianBinaryWriter : IDisposable
     {
+        #pragma warning disable IDE0032 // Disable the 'Use Auto Property' suggestion
+        private Stream stream;
+        #pragma warning restore IDE0032
+
+        private readonly byte[] buffer;
         private readonly bool leaveOpen;
 
-        public BigEndianBinaryWriter(Stream stream, bool leaveOpen) : base(stream)
+        /// <summary>
+        /// Initializes a new instance of the <see cref="BigEndianBinaryWriter"/> class.
+        /// </summary>
+        /// <param name="stream">The stream.</param>
+        /// <param name="leaveOpen">If set to <c>true</c> leave the stream open when disposing..</param>
+        /// <exception cref="ArgumentNullException">stream</exception>
+        public BigEndianBinaryWriter(Stream stream, bool leaveOpen)
         {
+            this.stream = stream ?? throw new ArgumentNullException(nameof(stream));
+            buffer = new byte[sizeof(double)];
             this.leaveOpen = leaveOpen;
         }
 
-        protected override void Dispose(bool disposing)
+        /// <summary>
+        /// Gets the underlying stream of the <see cref="BigEndianBinaryWriter"/>.
+        /// </summary>
+        /// <value>
+        /// The underlying stream of the <see cref="BigEndianBinaryWriter"/>.
+        /// </value>
+        /// <exception cref="ObjectDisposedException">The object has been disposed.</exception>
+        public Stream BaseStream
         {
-            if (!this.leaveOpen)
+            get
             {
-                base.Dispose(disposing);
+                if (stream == null)
+                {
+                    throw new ObjectDisposedException(nameof(BigEndianBinaryWriter));
+                }
+
+                // Force the stream to write any buffered data.
+                stream.Flush();
+
+                return stream;
             }
         }
 
-        public override void Write(char value)
+        /// <summary>
+        /// Performs application-defined tasks associated with freeing, releasing, or resetting unmanaged resources.
+        /// </summary>
+        public void Dispose()
         {
-            unsafe
+            if (!leaveOpen && stream != null)
             {
-                SwapBytes((byte*)&value, 2);
+                stream.Dispose();
+                stream = null;
             }
-            base.Write(value);
         }
 
-        public override void Write(short value)
+        /// <summary>
+        /// Writes a byte to the current stream.
+        /// </summary>
+        /// <param name="value">The value.</param>
+        /// <exception cref="ObjectDisposedException">The object has been disposed.</exception>
+        public void Write(byte value)
         {
-            unsafe
+            if (stream == null)
             {
-                SwapBytes((byte*)&value, 2);
+                throw new ObjectDisposedException(nameof(BigEndianBinaryWriter));
             }
-            base.Write(value);
+
+            stream.WriteByte(value);
         }
 
-        public override void Write(int value)
+        /// <summary>
+        /// Writes a 2-byte signed integer to the current stream in big endian byte order.
+        /// </summary>
+        /// <param name="value">The value.</param>
+        /// <exception cref="ObjectDisposedException">The object has been disposed.</exception>
+        public void Write(short value)
         {
-            unsafe
+            if (stream == null)
             {
-                SwapBytes((byte*)&value, 4);
+                throw new ObjectDisposedException(nameof(BigEndianBinaryWriter));
             }
-            base.Write(value);
+
+            buffer[0] = (byte)(value >> 8);
+            buffer[1] = (byte)value;
+
+            stream.Write(buffer, 0, 2);
         }
 
-        public override void Write(long value)
+        /// <summary>
+        /// Writes a 4-byte signed integer to the current stream in big endian byte order.
+        /// </summary>
+        /// <param name="value">The value.</param>
+        /// <exception cref="ObjectDisposedException">The object has been disposed.</exception>
+        public void Write(int value)
         {
-            unsafe
+            if (stream == null)
             {
-                SwapBytes((byte*)&value, 8);
+                throw new ObjectDisposedException(nameof(BigEndianBinaryWriter));
             }
-            base.Write(value);
+
+            buffer[0] = (byte)(value >> 24);
+            buffer[1] = (byte)(value >> 16);
+            buffer[2] = (byte)(value >> 8);
+            buffer[3] = (byte)value;
+
+            stream.Write(buffer, 0, 4);
         }
 
-        public override void Write(ushort value)
+        /// <summary>
+        /// Writes an 8-byte signed integer to the current stream in big endian byte order.
+        /// </summary>
+        /// <param name="value">The value.</param>
+        /// <exception cref="ObjectDisposedException">The object has been disposed.</exception>
+        public void Write(long value)
         {
-            unsafe
+            if (stream == null)
             {
-                SwapBytes((byte*)&value, 2);
+                throw new ObjectDisposedException(nameof(BigEndianBinaryWriter));
             }
-            base.Write(value);
+
+            buffer[0] = (byte)(value >> 56);
+            buffer[1] = (byte)(value >> 48);
+            buffer[2] = (byte)(value >> 40);
+            buffer[3] = (byte)(value >> 32);
+            buffer[4] = (byte)(value >> 24);
+            buffer[5] = (byte)(value >> 16);
+            buffer[6] = (byte)(value >> 8);
+            buffer[7] = (byte)value;
+
+            stream.Write(buffer, 0, 8);
         }
 
-        public override void Write(uint value)
+        /// <summary>
+        /// Writes a 2-byte unsigned integer to the current stream in big endian byte order.
+        /// </summary>
+        /// <param name="value">The value.</param>
+        /// <exception cref="ObjectDisposedException">The object has been disposed.</exception>
+        public void Write(ushort value)
         {
-            unsafe
+            if (stream == null)
             {
-                SwapBytes((byte*)&value, 4);
+                throw new ObjectDisposedException(nameof(BigEndianBinaryWriter));
             }
-            base.Write(value);
+
+            buffer[0] = (byte)(value >> 8);
+            buffer[1] = (byte)value;
+
+            stream.Write(buffer, 0, 2);
         }
 
-        public override void Write(ulong value)
+        /// <summary>
+        /// Writes a 4-byte unsigned integer to the current stream in big endian byte order.
+        /// </summary>
+        /// <param name="value">The value.</param>
+        /// <exception cref="ObjectDisposedException">The object has been disposed.</exception>
+        public void Write(uint value)
         {
-            unsafe
+            if (stream == null)
             {
-                SwapBytes((byte*)&value, 8);
+                throw new ObjectDisposedException(nameof(BigEndianBinaryWriter));
             }
-            base.Write(value);
+
+            buffer[0] = (byte)(value >> 24);
+            buffer[1] = (byte)(value >> 16);
+            buffer[2] = (byte)(value >> 8);
+            buffer[3] = (byte)value;
+
+            stream.Write(buffer, 0, 4);
         }
 
-        public override void Write(double value)
+        /// <summary>
+        /// Writes an 8-byte unsigned integer to the current stream in big endian byte order.
+        /// </summary>
+        /// <param name="value">The value.</param>
+        /// <exception cref="ObjectDisposedException">The object has been disposed.</exception>
+        public void Write(ulong value)
         {
-            unsafe
+            if (stream == null)
             {
-                SwapBytes((byte*)&value, 8);
+                throw new ObjectDisposedException(nameof(BigEndianBinaryWriter));
             }
-            base.Write(value);
+
+            buffer[0] = (byte)(value >> 56);
+            buffer[1] = (byte)(value >> 48);
+            buffer[2] = (byte)(value >> 40);
+            buffer[3] = (byte)(value >> 32);
+            buffer[4] = (byte)(value >> 24);
+            buffer[5] = (byte)(value >> 16);
+            buffer[6] = (byte)(value >> 8);
+            buffer[7] = (byte)value;
+
+            stream.Write(buffer, 0, 8);
+        }
+
+        /// <summary>
+        /// Writes a 4-byte floating point value to the current stream in big endian byte order.
+        /// </summary>
+        /// <param name="value">The value.</param>
+        /// <exception cref="ObjectDisposedException">The object has been disposed.</exception>
+        public unsafe void Write(float value)
+        {
+            if (stream == null)
+            {
+                throw new ObjectDisposedException(nameof(BigEndianBinaryWriter));
+            }
+
+            int temp = *(int*)&value;
+
+            buffer[0] = (byte)(temp >> 24);
+            buffer[1] = (byte)(temp >> 16);
+            buffer[2] = (byte)(temp >> 8);
+            buffer[3] = (byte)temp;
+
+            stream.Write(buffer, 0, 4);
+        }
+
+        /// <summary>
+        /// Writes an 8-byte floating point value to the current stream in big endian byte order.
+        /// </summary>
+        /// <param name="value">The value.</param>
+        /// <exception cref="ObjectDisposedException">The object has been disposed.</exception>
+        public unsafe void Write(double value)
+        {
+            if (stream == null)
+            {
+                throw new ObjectDisposedException(nameof(BigEndianBinaryWriter));
+            }
+
+            long temp = *(long*)&value;
+
+            buffer[0] = (byte)(temp >> 56);
+            buffer[1] = (byte)(temp >> 48);
+            buffer[2] = (byte)(temp >> 40);
+            buffer[3] = (byte)(temp >> 32);
+            buffer[4] = (byte)(temp >> 24);
+            buffer[5] = (byte)(temp >> 16);
+            buffer[6] = (byte)(temp >> 8);
+            buffer[7] = (byte)temp;
+
+            stream.Write(buffer, 0, 8);
+        }
+
+        /// <summary>
+        /// Writes the specified byte array to the current stream.
+        /// </summary>
+        /// <param name="bytes">The bytes.</param>
+        /// <exception cref="ArgumentNullException"><paramref name="bytes"/> is null.</exception>
+        /// <exception cref="ObjectDisposedException">The object has been disposed.</exception>
+        public void Write(byte[] bytes)
+        {
+            if (bytes == null)
+            {
+                throw new ArgumentNullException(nameof(bytes));
+            }
+            if (stream == null)
+            {
+                throw new ObjectDisposedException(nameof(BigEndianBinaryWriter));
+            }
+
+            stream.Write(bytes, 0, bytes.Length);
+        }
+
+        /// <summary>
+        /// Writes the specified number of bytes to the current stream, starting from a specified point in the byte array.
+        /// </summary>
+        /// <param name="bytes">The bytes.</param>
+        /// <param name="offset">The starting offset in the array.</param>
+        /// <param name="count">The count.</param>
+        /// <exception cref="ObjectDisposedException">The object has been disposed.</exception>
+        public void Write(byte[] bytes, int offset, int count)
+        {
+            if (stream == null)
+            {
+                throw new ObjectDisposedException(nameof(BigEndianBinaryWriter));
+            }
+
+            stream.Write(bytes, offset, count);
         }
 
         //////////////////////////////////////////////////////////////////
 
+        /// <summary>
+        /// Writes a rectangle comprised of 2-byte signed integers.
+        /// </summary>
+        /// <param name="rect">The rect.</param>
+        /// <exception cref="ObjectDisposedException">The object has been disposed.</exception>
         public void WriteInt16Rectangle(Rectangle rect)
         {
+            if (stream == null)
+            {
+                throw new ObjectDisposedException(nameof(BigEndianBinaryWriter));
+            }
+
             Write((short)rect.Top);
             Write((short)rect.Left);
             Write((short)rect.Bottom);
             Write((short)rect.Right);
         }
 
+        /// <summary>
+        /// Writes a rectangle comprised of 4-byte signed integers.
+        /// </summary>
+        /// <param name="rect">The rect.</param>
+        /// <exception cref="ObjectDisposedException">The object has been disposed.</exception>
         public void WriteInt32Rectangle(Rectangle rect)
         {
+            if (stream == null)
+            {
+                throw new ObjectDisposedException(nameof(BigEndianBinaryWriter));
+            }
+
             Write(rect.Top);
             Write(rect.Left);
             Write(rect.Bottom);
             Write(rect.Right);
         }
 
+        /// <summary>
+        /// Writes a length-prefixed UTF-16 string.
+        /// </summary>
+        /// <param name="value">The value.</param>
+        /// <exception cref="ObjectDisposedException">The object has been disposed.</exception>
         public void WriteUnicodeString(string value)
         {
+            if (stream == null)
+            {
+                throw new ObjectDisposedException(nameof(BigEndianBinaryWriter));
+            }
+
             Write(checked(value.Length + 1));
             Write(Encoding.BigEndianUnicode.GetBytes(value));
             // The string is always null-terminated.
             Write((ushort)0);
         }
-
-        //////////////////////////////////////////////////////////////////
-
-        private static unsafe void SwapBytes(byte* ptr, int nLength)
-        {
-            for (long i = 0; i < nLength / 2; ++i)
-            {
-                byte t = *(ptr + i);
-                *(ptr + i) = *(ptr + nLength - i - 1);
-                *(ptr + nLength - i - 1) = t;
-            }
-        }
-
     }
 }
